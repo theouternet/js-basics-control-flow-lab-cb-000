a34b05718e0aa3342494d729f11eac44dc2b7cff
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-19 03:47:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+if (feet < 400) {
+  return 'This one is on me!'
+}
+  else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city == 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
+  
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  let response;
+  switch (generosity) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
+  
 }
