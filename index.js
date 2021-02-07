932f71ee7c0c396d47b6ebe796e14fc21a74a358
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 07:34:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-function scuberGreetingForFeet(feet){
-if (feet < 400) {
-  return 'This one is on me!'
-}
-  else if (feet > 2000 && feet < 2500) {
+function scuberGreetingForFeet(dist) {
+  if (dist < 200) {
+    return 'This one is on me!'
+  }
+  else if (dist > 2000 && dist <= 2500) {
     return 'I will gladly take your thirty bucks.'
   }
-  else {
+  else if (dist > 2500) {
     return 'No can do.'
   }
+  
 }
 
-function ternaryCheckCity(city){
-  if (city == 'NYC') {
-    return 'Ok, sounds good.'
-  }
-  else {
-    return 'No go.'
-  }
-  
-  
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(generosity){
-  let response;
-  switch (generosity) {
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
     case 'generous':
-      response = 'Thank you so much.';
-      break;
+      return "Thank you so much."
     case 'not as generous':
-      response = 'Thank you.';
-      break;
-    default:
-      response = 'Bye.';
+      return "Thank you."
+    default :
+      return "Bye."
   }
-  return response;
-  
 }
+
