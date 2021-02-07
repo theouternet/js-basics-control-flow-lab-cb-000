d461e52e6739dcb5b6e252c9fe7eb754fc80037c
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 07:28:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(dist) {
   if (dist < 200) {
     return 'This one is on me!'
   }
-  else if (dist > 2000) {
+  else if (dist > 2000 && dist <= 2500) {
     return 'I will gladly take your thirty bucks.'
   }
   else if (dist > 2500) {
