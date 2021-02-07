a29db7c8411229e7a6482f5ca91db00dd83158a9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 07:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     case 'generous':
       return "Thank you so much."
-      
+    case 'not as generous':
+      return "Thank you."
+    default
   }
 }
 
